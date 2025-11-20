83c05fac6c91cc9276e952676137ea736c53aedf
Adem Mekonnen <adem.se2010@gmail.com>
2025-11-20 08:32:06 +0300

    added prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,27 @@
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+} else {
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
+  prisma = (global as any).prisma;
+}
+
+export default prisma;
